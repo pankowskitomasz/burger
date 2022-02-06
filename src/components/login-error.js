6ce337dd2e83ebba11fa710b40a87cf98d3a56e1
tomaszpankowski <--get-all>
2022-02-06 14:12:55 +0100

    added backend

--- src/components/login-error.js
+++ src/components/login-error.js
@@ -5,7 +5,7 @@ import Col from "../../node_modules/react-bootstrap/Col";
 import Button from "../../node_modules/react-bootstrap/Button";
 import Card from "../../node_modules/react-bootstrap/Card";
 
-class MessageSent extends Component {
+class LoginError extends Component {
     hndBack(){
         this.props.backNav("form");
     }
@@ -14,14 +14,14 @@ class MessageSent extends Component {
             <Container fluid className={"login-form align-items-center p-0 py-5 bg-white d-flex minh-footer-adj " + this.props.classExt}>
                 <Row className="mx-auto text-center w-100">
                     <Col xs={10} md={6} className="mx-auto p-0 d-flex align-items-center">
-                        <Card className="mx-auto bg-form border-dark-brw shadow bg-dark opacity-9">
-                            <Card.Header className="text-dark-brw text-start border-dark-brw">
-                                Message sent!
+                        <Card className="mx-auto border border-dark-brw rounded bg-contact shadow text-dark-brw">
+                            <Card.Header className="text-dark-brw fw-bold text-start border-dark-brw">
+                                Login error!
                             </Card.Header>
-                            <Card.Body className="text-center text-dark-brw bg-form p-5">
-                                <p className="lead">Message was sent!</p>
-                                <Button variant={"outline-light border-dark-brw text-dark-brw rounded-pill"} onClick={this.hndBack.bind(this)}>
-                                    back to Form
+                            <Card.Body className="text-center text-dark-brw p-5">
+                                <p className="lead">Incorrect login or password!</p>
+                                <Button variant={"outline-light border-dark-brw text-dark-brw"} onClick={this.hndBack.bind(this)}>
+                                    OK
                                 </Button>
                             </Card.Body>
                         </Card>
@@ -32,4 +32,4 @@ class MessageSent extends Component {
     }
 }
 
-export default MessageSent;
+export default LoginError;
--- src/config.js
+++ src/config.js
@@ -27,12 +27,17 @@ const appCfg = {
 };
 
 const APP_LINKS = {
-    messages: "message.php",
+    messages: "http://localhost/burger/public/message.php",
     register: "register.php",
-    users: "user.php"
+    users: "http://localhost/burger/public/user.php"
+};
+
+const msgCfg = {
+    pageSize: 10
 };
 
 export {
     appCfg,
-    APP_LINKS
+    APP_LINKS,
+    msgCfg
 }
